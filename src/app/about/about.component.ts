8d04c6603ef96a8007248077ceeae71683d822b8
Shehriyar Qureshi <SShehriyar266@gmail.com>
2019-06-15 23:53:24 +0500

    Use ngFor to display skills

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -7,6 +7,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
+  public frontendSkills = [
+    "Angular",
+    "HTML",
+    "CSS",
+    "pyGTK"
+  ];
+
+  public backendSkills = [
+    "Java",
+    "Spring Boot",
+    "Spring Security",
+    "Python",
+    "JDBC",
+    "Hibernate ORM",
+  ];
+
+  public databaseSkills = [
+    "PostgreSQL",
+    "SQLite",
+    "PostGIS",
+    "pgAdmin"
+  ];
+
+  public otherSkills = [
+    "RESTful Web Services",
+    "Geographic Information System",
+    "Linux",
+    "Bash Scripting",
+    "Version Control (Git/Github/Bitbucket)",
+    "Apache Tomcat",
+    "JSON",
+    "XML",
+    "Regular Expressions",
+    "Vim"
+  ]
+
   constructor() { }
 
   ngOnInit() {
