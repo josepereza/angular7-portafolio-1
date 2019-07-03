02387513e298963f611dcb6bb1bc6c87c33fa225
Shehriyar Qureshi <SShehriyar266@gmail.com>
2019-07-03 19:16:14 +0500

    add typing animation on title and subtitle

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, SimpleChanges } from '@angular/core';
 import { AboutComponent } from '../about/about.component';
+import { async, delay } from 'q';
+import { Observable, of, Subscription } from 'rxjs';
+
+declare var TypeIt: any;
 
 @Component({
   selector: 'app-home',
@@ -8,45 +12,52 @@ import { AboutComponent } from '../about/about.component';
 })
 export class HomeComponent implements OnInit {
 
+  subscription: Subscription;
+
+  isTitleTyped = false;
+
   projects: any = [
     {
-      "name": "historia",
-      "type": "Open Source",
-      "description": `A python program that uses python's built-in sqlite3
+      name: 'historia',
+      type: 'Open Source',
+      description: `A python program that uses python's built-in sqlite3
       module to implement support for temporal tables in SQLite`,
-      "link": "https://www.github.com/ShehriyarQureshi/historia",
-      "image": "assets/images/temporal-table.png"
+      link: 'https://www.github.com/ShehriyarQureshi/historia',
+      image: 'assets/images/temporal-table.png'
     },
     {
-      "name": "Tuition Manager",
-      "type": "Open Source",
-      "image": "assets/images/tuition-manager.png",
-      "description": `This was my first ever big programming project. I started
+      name: 'Tuition Manager',
+      type: 'Open Source',
+      image: 'assets/images/tuition-manager.png',
+      description: `This was my first ever big programming project. I started
       learning to code in Python and I developed this project to get an
       understanding of programming basics and how I/O and stuff works`,
-      "link": "https://www.github.com/ShehriyarQureshi/tuition-manager"
+      link: 'https://www.github.com/ShehriyarQureshi/tuition-manager'
     },
     {
-      "name": "employeeManager",
-      "type": "Open Source",
-      "image": "assets/images/employeeManager.png",
-      "description": `My first Java program that uses JSF, Hibernate & Spring
+      name: 'employeeManager',
+      type: 'Open Source',
+      image: 'assets/images/employeeManager.png',
+      description: `My first Java program that uses JSF, Hibernate & Spring
       Security. Not proud of it except those Pokemon`,
-      "link": "https://www.github.com/ShehriyarQureshi/employeeManager"
+      link: 'https://www.github.com/ShehriyarQureshi/employeeManager'
 
     },
     {
-      "name": "Mobile Connect Profile",
-      "type": "Open Source",
-      "description": `Designed & Developed the base for GSMA Mobile Connect Profile prototype`,
-      "link": "https://www.github.com/ShehriyarQureshi/mobile-connect-profile",
-      "image": "assets/images/gsma-mobile-connect.jpg"
+      name: 'Mobile Connect Profile',
+      type: 'Open Source',
+      description: `Designed & Developed the base for GSMA Mobile Connect Profile prototype`,
+      link: 'https://www.github.com/ShehriyarQureshi/mobile-connect-profile',
+      image: 'assets/images/gsma-mobile-connect.jpg'
     }
   ];
 
-  constructor(public about: AboutComponent) { }
+  constructor(public about: AboutComponent) {
+  }
 
   ngOnInit() {
+    this.typeTitle();
+    // this.typeWhatIDo();
   }
 
   goToLink(link: string): void {
@@ -55,5 +66,44 @@ export class HomeComponent implements OnInit {
 
   what(event: any): void {
     console.log('hi', event);
+  }
+
+  public typeTitle(): void {
+    const typeTitle = new TypeIt('#title', {
+      strings: this.about.about.status,
+      speed: 70,
+      waitUntilVisible: true,
+      lifeLike: true,
+      cursorChar: '_',
+      afterComplete: (instance) => {
+        this.typeWhatIDo();
+      }
+    }).go();
+
+  }
+
+  public typeWhatIDo(): void {
+    const typeDeliverables = new TypeIt('#deliverables', {
+      speed: 50,
+      waitUntilVisible: true,
+      lifeLike: true,
+      cursorChar: '_',
+    })
+      .type('I create ')
+      .pause(500)
+      .type('websites,')
+      .pause(1500)
+      .delete(9)
+      .type('web applications,')
+      .pause(1500)
+      .delete(17)
+      .type('desktop applications,')
+      .pause(1500)
+      .delete(28)
+      .type('develop Software')
+      .pause(500)
+      .type('.');
+
+    typeDeliverables.go();
   }
 }
